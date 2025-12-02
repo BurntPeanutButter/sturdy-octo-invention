e07d2febbe045a4ea7ca497d0853659e0adf1f93
BurntPeanutButter <markus.lombard10@gmail.com>
2025-12-02 20:31:15 +0200

    Kyk ROnd 2

--- horror/player.gd
+++ horror/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody3D
 
-var sensitivity: float = 0.3
-var limitKamera: float = 80.0
+var speed: float = 500.0
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		# Beweeg links en regs
-		rotation_degrees.y -= event.relative.x * sensitivity
-		#Beweeg op en af
-		%Camera3D.rotation_degrees.x -= event.relative.y * sensitivity
-		#Limit Kamera sodat dit nie te ver op/af kyk nie
+		rotation_degrees.y -= event.relative.x * 0.3
+		#get_node(Camera3D) Main
+		# $Camera3D -> Shortcut
+		%Camera3D.rotation_degrees.x -= event.relative.y * 0.1# Right Click Kamera dan Unique name
+		#%gun_model # Omdat unique is dit nie meer #Camera3D/gun_model
 		%Camera3D.rotation_degrees.x = clamp(
-			%Camera3D.rotation_degrees.x, -limitKamera, limitKamera
+			%Camera3D.rotation_degrees.x, -80.0, 80.0
 			)
+	# As dit inset kry van iets anders as die muis, bv. Keyboard
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
